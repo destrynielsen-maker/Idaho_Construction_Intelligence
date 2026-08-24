76ca1bf46cd6d5731f316ded0001c07c93ce1b82
destrynielsen-maker <destrynielsen@gmail.com>
2026-08-24 16:10:08 -0600

    Wire Eagle into Idaho collection pipeline

--- src/idaho_permits/pipeline.py
+++ src/idaho_permits/pipeline.py
@@ -5,12 +5,13 @@ from pathlib import Path
 from .classify import classify_permit
 from .collectors.boise import BoiseDevelopmentCollector
 from .collectors.coeur_dalene import CoeurDAleneCollector
+from .collectors.eagle import EaglePermitCollector
 from .collectors.kootenai_county import KootenaiCountyCollector
 from .collectors.report_pages import MeridianCollector,NampaCollector
 from .dashboard import write_public_data
 from .feeds import write_all_feeds
 from .storage import load_permits,save_permits
-COLLECTORS=[BoiseDevelopmentCollector(),CoeurDAleneCollector(),KootenaiCountyCollector(),MeridianCollector(),NampaCollector()]
+COLLECTORS=[BoiseDevelopmentCollector(),EaglePermitCollector(),CoeurDAleneCollector(),KootenaiCountyCollector(),MeridianCollector(),NampaCollector()]
 
 def _site_base_url():
     x=os.getenv('SITE_BASE_URL','').strip()
