05f0714926d694de7f626b3192982a058dcd8c9d
destrynielsen-maker <destrynielsen@gmail.com>
2026-08-24 19:14:54 -0600

    Test Caldwell direct report URL candidates

--- tests/test_caldwell_discovery.py
+++ tests/test_caldwell_discovery.py
@@ -1,25 +1,20 @@
 import unittest
-from idaho_permits.collectors.caldwell import report_links_from_page
-
-HTML = '''
-<html><body>
-<a href="/files/building/april-2026.pdf">April 2026</a>
-<a href="/files/building/may-2026.pdf">May 2026</a>
-<a href="/files/building/june-2026.pdf">June 2026</a>
-<a href="/not-a-report.html">June 2026 overview</a>
-</body></html>
-'''
+from datetime import date
+from idaho_permits.collectors.caldwell import candidate_report_urls
 
 class CaldwellDiscoveryTests(unittest.TestCase):
-    def test_discovers_and_orders_official_monthly_pdf_reports(self):
-        rows = report_links_from_page(HTML, 'https://www.cityofcaldwell.org/reports')
-        self.assertEqual([r['label'] for r in rows], ['April 2026', 'May 2026', 'June 2026'])
-        self.assertEqual(rows[-1]['report_date'], '2026-06-01')
-        self.assertEqual(rows[-1]['url'], 'https://www.cityofcaldwell.org/files/building/june-2026.pdf')
+    def test_builds_current_and_previous_month_report_urls(self):
+        rows = candidate_report_urls(date(2026, 8, 24), months_back=4)
+        self.assertEqual([r['label'] for r in rows], [
+            'August 2026', 'July 2026', 'June 2026', 'May 2026'
+        ])
+        self.assertTrue(rows[1]['url'].endswith('/july-2026-building-report.pdf'))
 
-    def test_ignores_non_pdf_month_links(self):
-        rows = report_links_from_page('<a href="/june">June 2026</a>')
-        self.assertEqual(rows, [])
+    def test_rolls_back_across_year_boundary(self):
+        rows = candidate_report_urls(date(2026, 1, 5), months_back=3)
+        self.assertEqual([r['label'] for r in rows], [
+            'January 2026', 'December 2025', 'November 2025'
+        ])
 
 if __name__ == '__main__':
     unittest.main()
