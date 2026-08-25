a93f32c36a7ac49968b82e599a8a27af16f9e38d
destrynielsen-maker <destrynielsen@gmail.com>
2026-08-24 19:12:17 -0600

    Wire Caldwell official report discovery

--- src/idaho_permits/pipeline.py
+++ src/idaho_permits/pipeline.py
@@ -4,7 +4,7 @@ from datetime import datetime,timezone
 from pathlib import Path
 from .classify import classify_permit
 from .collectors.boise import BoiseDevelopmentCollector
-from .collectors.caldwell import CaldwellCitizenserveDiscoveryCollector
+from .collectors.caldwell import CaldwellReportDiscoveryCollector
 from .collectors.canyon_county import CanyonCountyPermitCollector
 from .collectors.coeur_dalene import CoeurDAleneCollector
 from .collectors.eagle import EaglePermitCollector
@@ -13,7 +13,7 @@ from .collectors.report_pages import MeridianCollector,NampaCollector
 from .dashboard import write_public_data
 from .feeds import write_all_feeds
 from .storage import load_permits,save_permits
-COLLECTORS=[BoiseDevelopmentCollector(),EaglePermitCollector(),CanyonCountyPermitCollector(),CaldwellCitizenserveDiscoveryCollector(),CoeurDAleneCollector(),KootenaiCountyCollector(),MeridianCollector(),NampaCollector()]
+COLLECTORS=[BoiseDevelopmentCollector(),EaglePermitCollector(),CanyonCountyPermitCollector(),CaldwellReportDiscoveryCollector(),CoeurDAleneCollector(),KootenaiCountyCollector(),MeridianCollector(),NampaCollector()]
 
 def _site_base_url():
     x=os.getenv('SITE_BASE_URL','').strip()
